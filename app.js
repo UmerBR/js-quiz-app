4465fe04fefa106ae1ccfb769b7c3d15822f032d
UmerBR <ru.raja.umer@gmail.com>
2024-06-22 17:38:23 +0500

    quiz questions added in app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const quizData = [
+    {
+        question: "What does CPU stand for?",
+        options: ["Central Processing Unit", "Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+        answer: "Central Processing Unit"
+    },
+    {
+        question: "What does RAM stand for?",
+        options: ["Random Access Memory", "Random Anything Memory", "Run Access Memory", "Read Access Memory"],
+        answer: "Random Access Memory"
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Hyperlink Markup Language"],
+        answer: "Hypertext Markup Language"
+    }
+];
